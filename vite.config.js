c958ef1d27f3f016d501c1aef5f6e630e427e235
naruapon <naruapon@gmail.com>
2026-02-02 09:53:59 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-// update
 export default defineConfig({
   base: '/',
   plugins: [vue()],
